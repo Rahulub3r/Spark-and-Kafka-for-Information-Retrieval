ee67ea42d19a7215786cc89f557650d39b371e98
rahull33t <rahulkumar.goyal@utdallas.edu>
2018-04-26 13:55:21 -0500

    Added three function definitions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -71,7 +71,20 @@ object Main {
     * @param maxOffsets maximum number of offsets per trigger
     * @return a streaming dataframe
     */
-  def ingestKafkaTopic(spark: SparkSession, bootstrap: String, topic: String, startingOffsets: String, maxOffsets: Long): DataFrame = ???
+  def ingestKafkaTopic(spark: SparkSession, bootstrap: String, topic: String, startingOffsets: String, maxOffsets: Long): DataFrame = {
+    spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers",bootstrap)
+      .option("startingOffsets", startingOffsets)
+      .option("maxOffsetsPerTrigger", maxOffsets)
+      .option("subscribe",topic)
+      .load()
+      .select(
+        col("key").cast("string"),
+        col("value").cast("string")
+      )
+  }
 
   /**
     * This method extracts data from the records coming from Kafka
@@ -91,7 +104,26 @@ object Main {
     * @param df the input dataframe
     * @return resulting dataframe
     */
-  def extractValues(df: DataFrame): DataFrame = ???
+  def extractValues(df: DataFrame): DataFrame = {
+    df.withColumn("tmp", split(col("value"), ","))
+      .select(
+        col("key").as("source"),
+        col("tmp").getItem(0).cast("integer").as("year"),
+        col("tmp").getItem(1).cast("integer").as("month"),
+        col("tmp").getItem(2).cast("integer").as("day"),
+        col("tmp").getItem(3).cast("integer").as("hour"),
+        col("tmp").getItem(5).cast("double").as("pm"),
+        col("tmp").getItem(11).cast("double").as("temp")
+      )
+      .withColumn(
+        "timestamp",
+        unix_timestamp(
+          format_string("%04d-%02d-%02d %02d:00:00", col("year"), col("month"), column("day"), column("hour"))
+        ).cast("timestamp")
+      )
+      .drop("year", "month", "day", "hour")
+      .filter(col("pm").isNotNull)
+  }
 
   /**
     * This method calculates the top pollution event by week and source.
@@ -113,6 +145,16 @@ object Main {
     * @param df the input dataframe
     * @return the resulting dataframe
     */
-  def calculateTopPollutionEventsPerWeek(df: DataFrame): DataFrame = ???
-
+  def calculateTopPollutionEventsPerWeek(df: DataFrame): DataFrame = {
+    import df.sparkSession.implicits._
+    df
+      .withWatermark("timestamp", "14 days")
+      .groupBy($"source", window($"timestamp","7 days"))
+      .agg(min("timestamp").alias("start_timestamp"),
+        max("timestamp").alias("end_timestamp"),
+        max("pm").alias("max_pm"),
+        avg("temp").alias("mean_temp"))
+      .withColumn("mean_temp", round($"mean_temp"))
+      .select("source", "start_timestamp", "end_timestamp", "max_pm", "mean_temp")
+  }
 }
